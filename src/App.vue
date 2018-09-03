5e0ce5d91dda2274957728f36af29ea8fdedd90d
MaoYaming <249945984@qq.com>
2018-09-03 11:40:04 +0800

    登录路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    
     <router-view/>
   </div>
 </template>
 
 <script>
+
+// http://192.168.136.83:8888/api/private/v1/users
 export default {
   name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>    
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
 
 Vue.use(Router)
-
+import Login from '@/views/Login.vue'
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        name: 'login',
+        path: '/login',
+        component: Login
+    }]
 })
