54bb12a437c6bf41dd6c0085ccea9ef0d7a1e7d7
MaoYaming <249945984@qq.com>
2018-09-06 16:29:24 +0800

    树状图

--- src/views/rights/Roles.vue
+++ src/views/rights/Roles.vue
@@ -57,26 +57,52 @@
             <el-table-column label="操作">
                 <template slot-scope="scope">
                     <el-button type="primary" icon="el-icon-edit" size="mini" plain></el-button>
-                    <el-button type="success" icon="el-icon-check" size="mini" plain></el-button>
+                    <el-button @click="handleClick" type="success" icon="el-icon-check" size="mini" plain></el-button>
                     <el-button type="danger" icon="el-icon-delete" size="mini" plain></el-button>
                 </template>
             </el-table-column>
         </el-table>
+        <!-- 对话框 -->
+        <el-dialog
+            title="提示"
+            :visible.sync="dialogVisible">
+            <span>
+                <el-tree
+                default-expand-all
+                :props="defaultProps"
+                :data="data"
+                show-checkbox>
 
+                </el-tree>
+            </span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
     </el-card>
 </template>
 <script>
     export default {
       data() {
           return {
-              tableData:[]
+              tableData: [],
+              dialogVisible: false,
+              //提供绑定的数据
+              data: [],
+              defaultProps: {
+                  //树上的节点对象的属性
+                  label:'authName',
+                    //   子节点对象的属性
+                  children:'children'
+              }
           }
       },
       created() {
           this.loadData()
       },
       methods: {
-          loadData() {
+            loadData() {
                 this.$http.get('roles')
                     .then((response)=>{
                         const { meta: {msg, status}} = response.data
@@ -86,20 +112,28 @@
                             this.$message.error(msg)
                         }
                     })
-          },
-          handleChose(role, rightId) {
-              this.$http.delete(`roles/${role.id}/rights/${rightId}`)
-                .then((response) => {
-                    const { meta: {msg, status}} = response.data
-                    if(status === 200) {
-                        this.$message.success(msg)
-                        role.children = response.data.data
-                    }else {
-                        this.$message.error(msg)
-                    }
+            },
+           
+            handleChose(role, rightId) {
+                this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+                    .then((response) => {
+                        const { meta: {msg, status}} = response.data
+                        if(status === 200) {
+                            this.$message.success(msg)
+                            role.children = response.data.data
+                        }else {
+                            this.$message.error(msg)
+                        }
+                    })
+            },
+            handleClick() {
+            this.dialogVisible = true
+            this.$http.get('rights/tree')
+                .then((response)=>{
+                    this.data = response.data.data
                 })
-
-          }
+            }
+         
       }
     }
 </script>
